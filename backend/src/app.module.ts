2680f6d0e01056368cd2e3cfaed135c3f650b30b
omkrfig <omkrfig@gmail.com>
2025-04-19 15:56:54 -0400

    Back simple

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from './config/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>(DB_HOST),
+        port: configService.get<number>(DB_PORT),
+        username: configService.get<string>(DB_USERNAME),
+        password: configService.get<string>(DB_PASSWORD),
+        database: configService.get<string>(DB_DATABASE),
+        entities: [],
+        synchronize: true,
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
